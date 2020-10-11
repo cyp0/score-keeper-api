466a8067630c7017262f39fae203e03c6d3b563e
Cyp0 <edgar-sanchez11@hotmail.com>
2020-10-10 19:49:35 -0500

    Added Home controller

---
+++ src/main/java/com/score_keeper/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.score_keeper.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "")
+public class HomeController {
+    @GetMapping
+    public String response(){
+        return "Score Keeper API";
+    }
+}
